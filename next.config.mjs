de9074565abe9b0779df4eed15c93e1fdcba6e35
Tubagus Ariq Naufal <64489324+ariqtb@users.noreply.github.com>
2024-07-09 11:46:13 +0700

    Update next.config.mjs
    configure base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/2048-in-react",
+  basePath: "/myportfolio",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
